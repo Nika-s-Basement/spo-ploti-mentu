e0d1f58903e8cc6fb5f36196b2b005b5465a7e31
Iiya Zakharov <2146376@gmail.com>
2024-05-18 20:02:02 +0300

    уиииииииииииииииииииииииииии

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import re
 import uvicorn
 from fastapi import FastAPI, HTTPException
@@ -179,4 +180,5 @@ async def get_dtp_app(dtp: GetDtp):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)

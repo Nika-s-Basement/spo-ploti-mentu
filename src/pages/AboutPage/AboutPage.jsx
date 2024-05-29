07ca68d19001e021c2732b0094b94ef21a01450d
Sagailyuk Mikhail <Sagailyuk@yandex.ru>
2024-05-29 21:46:19 +0300

    The 3 page

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Layout from '../../component/FrontBackReq/Layout';
+import styles from  './AboutPage.module.css';
+
 
 const AboutPage = () => {
+    function CampusList() {
+        const [campuses, setCampuses] = useState([]);
+        const [filter, setFilter] = useState('https://spo-ploti-mentu.onrender.com/get/all/gai');
+
+        useEffect(() => {
+            // Получение всех кампусов из базы данных
+            fetch('')
+                .then(response => response.json())
+                .then(data => setCampuses(data));
+        }, []);
+
+        function handleFilterChange(event) {
+            setFilter(event.target.value);
+        }
+
+        function handleFilterSubmit(event) {
+            event.preventDefault();
+            // Фильтрация кампусов по заданному критерию
+            setCampuses(campuses.filter(campus => campus.name.includes(filter)));
+        }
+    }
+
     return (
+
         <Layout>
-            <h1>
-                Миша работат!
-            </h1>
+            <div>
+                <form onSubmit={handleFilterSubmit}>
+                    <label htmlFor="filter-input">Filter by name:</label>
+                    <input type="text" id="filter-input" value={filter} onChange={handleFilterChange}/>
+                    <button type="submit">Filter</button>
+                </form>
+                <ul>
+                    {campuses.map(campus => (
+                        <li key={campus.id}>{campus.name}</li>
+                    ))}
+                </ul>
+            </div>
         </Layout>
     )
 }
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -4,6 +4,7 @@ import LoginForm from '../../component/LoginForm/LoginForm';
 import UserForm from '../../component/UserForm/UserForm';
 import UserReg from '../../component/UserReg/UserReg';
 import Button from '../../component/Button/Button';
+import styles from './LoginPage.module.css';
 
 const LoginPage = () => {
   const [showUserForm, setShowUserForm] = useState(false);
@@ -30,27 +31,29 @@ const LoginPage = () => {
   };
 
   return (
-    <Layout>
-      {!showUserForm && !showUserReg && (
-        <>
-          <LoginForm label = "Войти"/>
-          <Button onClick={handleDriverClick}>Войти как водитель</Button>
-        </>
-      )}
-      {showUserForm && (
-        <>
-          <UserForm />
-          <Button onClick={handleMentorClick}>Войти как мент</Button>
-          <Button onClick={handleRegisterClick}>Зарегистрироваться</Button>
-        </>
-      )}
-      {showUserReg && (
-        <>
-          <UserReg />
-          <Button onClick={handleCancelRegister}>Отмена</Button>
-        </>
-      )}
-    </Layout>
+      <div className={styles.Button}>
+        <Layout>
+          {!showUserForm && !showUserReg && (
+              <>
+                <LoginForm className={styles.Button} label = "Войти"/>
+                <Button className={styles.Button} onClick={handleDriverClick}>Войти как водитель</Button>
+              </>
+          )}
+          {showUserForm && (
+              <>
+                <UserForm className={styles.Button} />
+                <Button className={styles.Button} onClick={handleMentorClick}>Войти как мент</Button>
+                <Button className={styles.Button} onClick={handleRegisterClick}>Зарегистрироваться</Button>
+              </>
+          )}
+          {showUserReg && (
+              <>
+                <UserReg className={styles.Button} />
+                <Button className={styles.Button} onClick={handleCancelRegister}>Отмена</Button>
+              </>
+          )}
+        </Layout>
+      </div>
   );
 };
 

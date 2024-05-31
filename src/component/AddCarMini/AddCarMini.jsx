07aa3e08c343e44a1250f9f17d9d381bccbc6c56
Sagailyuk Mikhail <Sagailyuk@yandex.ru>
2024-05-31 23:33:23 +0300

    Настройка поиска

--- src/component/AddCarMini/AddCarMini.jsx
+++ src/component/AddCarMini/AddCarMini.jsx
@@ -1,43 +1,45 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import InputField from '../InputField/InputField';
 import styles from './AddCarMini.module.css';
 import Button from '../Button/Button';
 
-const AddCarMini = ({ onAddCar }) => {
-  const [license, setLicense] = useState('');
-  const [car_num, setCar_num] = useState('');
+const AddCarMini = ({onAddCar}) => {
+    const [license, setLicense] = useState('');
+    const [car_num, setCar_num] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-    const car = {
-      license: license,
-      car_num: car_num,
+        const car = {
+            license: license,
+            car_num: car_num,
+        };
+
+        onAddCar(car);
+
+        setLicense('');
+        setCar_num('');
     };
 
-    onAddCar(car);
-
-    setLicense('');
-    setCar_num('');
-  };
-
-  return (
-    <div onSubmit={handleSubmit}>
-      <InputField
-        label="Номер ТС"
-        type="number"
-        value={license}
-        onChange={(e) => setLicense(e.target.value)}
-      />
-      <InputField
-        label="Гос. номер"
-        type="text"
-        value={car_num}
-        onChange={(e) => setCar_num(e.target.value)}
-      />
-      <Button className={styles.addCarMiniButton} onClick={handleSubmit}>Сохранить авто</Button>
-    </div>
-  );
+    return (
+        <div onSubmit={handleSubmit}>
+            <InputField
+                label="Номер ТС"
+                type="number"
+                value={license}
+                onChange={(e) => setLicense(e.target.value)}
+            />
+            <InputField
+                label="Гос. номер"
+                type="text"
+                value={car_num}
+                onChange={(e) => setCar_num(e.target.value)}
+            />
+            <div className={styles.addCarMiniButton}>
+                <Button onClick={handleSubmit}>Сохранить авто</Button>
+            </div>
+        </div>
+    );
 };
 
 export default AddCarMini;
--- src/component/AddDTP/AddDTP.jsx
+++ src/component/AddDTP/AddDTP.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 import InputField from '../InputField/InputField';
 import AddCarMini from '../AddCarMini/AddCarMini';
@@ -6,80 +6,81 @@ import styles from './AddDTP.module.css';
 import Button from '../Button/Button'
 
 const AddDtp = () => {
-  const [describe, setDescribe] = useState('');
-  const [address, setAddress] = useState('');
-  const [date, setDate] = useState('');
-  const [photo, setPhoto] = useState('');
-  const [cars, setCars] = useState([]);
+    const [describe, setDescribe] = useState('');
+    const [address, setAddress] = useState('');
+    const [date, setDate] = useState('');
+    const [photo, setPhoto] = useState('');
+    const [cars, setCars] = useState([]);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-    const data = {
-      describe: describe,
-      cars: cars,
-      address: address,
-      date: date,
-      photo: photo
+        const data = {
+            describe: describe,
+            cars: cars,
+            address: address,
+            date: date,
+            photo: photo
+        };
+
+        const token = localStorage.getItem('userToken');
+        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+        axios.post('https://spo-ploti-mentu.onrender.com/add/dtp/', data)
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
     };
-    
-    const token = localStorage.getItem('userToken');
-    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
-    axios.post('https://spo-ploti-mentu.onrender.com/add/dtp/', data)
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
-  };
+    const handleAddCar = (car) => {
+        setCars([...cars, car]);
+    };
 
-  const handleAddCar = (car) => {
-    setCars([...cars, car]);
-  };
+    return (
+        <form className={styles.addDtpForm} onSubmit={handleSubmit}>
+            <InputField
+                label="Описание"
+                type="textarea"
+                value={describe}
+                onChange={(e) => setDescribe(e.target.value)}
+                className={styles.addDtpInput}
+            />
+            <InputField
+                label="Адрес"
+                type="text"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                className={styles.addDtpInput}
+            />
+            <InputField
+                label="Дата"
+                type="datetime-local"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+                className={styles.addDtpInput}
+            />
+            <InputField
+                label="Фото"
+                type="text"
+                value={photo}
+                onChange={(e) => setPhoto(e.target.value)}
+                className={styles.addDtpInput}
+            />
+            <AddCarMini onAddCar={handleAddCar}/>
+            <ul>
+                {cars.map((car, index) => (
+                    <li key={index}>
+                        Номер ТС: {car.license}, Гос. номер: {car.car_num}
+                    </li>
+                ))}
+            </ul>
+            <Button type="submit">Добавить ДТП</Button>
 
-  return (
-    <form className={styles.addDtpForm} onSubmit={handleSubmit}>
-      <InputField
-        label="Описание"
-        type="textarea"
-        value={describe}
-        onChange={(e) => setDescribe(e.target.value)}
-        className={styles.addDtpInput}
-      />
-      <InputField
-        label="Адрес"
-        type="text"
-        value={address}
-        onChange={(e) => setAddress(e.target.value)}
-        className={styles.addDtpInput}
-      />
-      <InputField
-        label="Дата"
-        type="datetime-local"
-        value={date}
-        onChange={(e) => setDate(e.target.value)}
-        className={styles.addDtpInput}
-      />
-      <InputField
-        label="Фото"
-        type="text"
-        value={photo}
-        onChange={(e) => setPhoto(e.target.value)}
-        className={styles.addDtpInput}
-      />
-      <AddCarMini onAddCar={handleAddCar} />
-      <ul>
-        {cars.map((car, index) => (
-          <li key={index}>
-            Номер ТС: {car.license}, Гос. номер: {car.car_num}
-          </li>
-        ))}
-      </ul>
-      <Button className={styles.addDtpButton} type="submit">Добавить ДТП</Button>
-    </form>
-  );
+        </form>
+    );
 };
 
 export default AddDtp;
